e7c47a499016a2e72fce9e5ff92be72e50af52f7
Josep <apuratepp@gmail.com>
2014-07-19 23:50:47 +0200

    fix wrong CORS approach

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :null_session
 
-	after_filter :set_access_control_headers
-
 	respond_to :json
 
 	def authenticate_user_from_token!
@@ -15,8 +13,4 @@ class ApplicationController < ActionController::Base
       sign_in user, store: false
     end
   end
-	def set_access_control_headers 
-		headers['Access-Control-Allow-Origin'] = '*' 
-		headers['Access-Control-Request-Method'] = '*' 
-	end
 end
